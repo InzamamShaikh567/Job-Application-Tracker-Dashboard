589860fdf73b646649daa4956148034ec0e350d0
Inzamamul Shaikh <45431765+InzamamShaikh567@users.noreply.github.com>
2026-05-12 21:18:58 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
   server: {
     port: 5173,
     proxy: {
-      '/api': {
+      '/v1': {
         target: 'http://localhost:8000',
         // target: import.meta.env.VITE_API_URL || 'http://localhost:8000',
         changeOrigin: true,
